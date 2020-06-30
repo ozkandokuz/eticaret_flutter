1fd24728ff587ded6dd5519296854f85024633e9
ozkan <ozkandokuz@gmail.com>
2020-06-30 19:42:40 +0300

    4

---
+++ projects/megakitap_debug/android/app/src/main/kotlin/com/megakitapdebug/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.megakitapdebug.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projects/yinyogashop/android/app/src/main/kotlin/com/yinyogashop/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yinyogashop.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
